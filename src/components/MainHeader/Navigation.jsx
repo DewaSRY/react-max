0d92650def80b728e43d4e76568dccb43f072119
DewaSRY <sdewa6645@gmail.com>
2023-02-18 18:52:38 +0800

    need to fix

---
+++ src/components/MainHeader/Navigation.jsx
@@ -0,0 +1,24 @@
+import { useContext } from "react";
+
+import AuthContext from "../../store/auth-context";
+import classes from "./Navigation.module.css";
+
+export default function Navigation() {
+  const { isLoggedIn, onLogout } = useContext(AuthContext);
+  if (!isLoggedIn) return;
+  return (
+    <nav className={classes.nav}>
+      <ul>
+        <li>
+          <a href="/">Users</a>
+        </li>
+        <li>
+          <a href="/">Admin</a>
+        </li>
+        <li>
+          <button onClick={onLogout}>Logout</button>
+        </li>
+      </ul>
+    </nav>
+  );
+}
